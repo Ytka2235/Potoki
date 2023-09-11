f6c504ebd6c0f6352882d16c6abd0e3ff9fd05ca
admin_0 <admin_0@WIN-R1LE9N2JJ8S>
2023-09-11 12:53:16 +0300

    Добавьте файлы проекта.

---
+++ Potoki/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Timers;
+
+internal class Program
+{
+    public static int counter = 0;
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Задание 1");
+        System.Timers.Timer timer = new System.Timers.Timer(1000);
+        timer.Elapsed += Start;
+        timer.Start();
+        bool flag = true;
+        while (flag)
+        {
+            if (counter == 10)
+            {
+                flag = false;
+                timer.Stop();
+            }
+        }
+        AllInfoProcess();
+        Console.ReadKey();
+
+    }
+
+    private static void Start(Object source, ElapsedEventArgs e)
+    {
+        Process myProc = null;
+        
+        try
+        {
+            myProc = Process.Start("Notepad.exe");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        counter++;
+    }
+    static void AllInfoProcess()
+    {
+        var myProcess = from proc in Process.GetProcesses(".")
+                        orderby proc.Id
+                        select proc;
+        Console.WriteLine("\n*** Текущие процессы ***\n");
+        foreach (var p in myProcess)
+            Console.WriteLine("-> PID: {0}\tStartTime: {1}\tName: {2}", p.Id,p.StartTime, p.ProcessName);
+    }
+
+
+}
